e47a7e6767f82f5a25beb7f47f1fb3426fec7f3a
raghavender666 <kr2824@srmist.edu.in>
2025-05-04 15:06:18 -0500

    Initial commit: Added backend project files

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Product;
@@ -56,5 +58,12 @@ public class ProductService {
     Product productcur=productid.get();
     repo.delete(productcur);
   }
+
+
+  
+  public Page<Product> getAllProducts(Pageable pageable) {
+      return repo.findAll(pageable);
+  }
+  
   
 }
